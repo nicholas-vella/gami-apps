6d5a9c5176140c9000d3b16908d73e5919f5e3b8
Luke Montgomery <luke.montgomery@hotmail.com>
2019-08-10 17:05:54 +1000

    satisfy heroku start

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,21 @@ import { Zelem } from './zelem';
 
 
 function main() {
+    const express = require('express')
+    const app = express();
+
     const zel = new Zelem();
 
     (async () => {
         await zel.start();
         console.log('Connected');
     })();
+    
+    app.get('/', function (req, res) {
+        res.send('Sup');
+    });
+    
+    app.listen(process.env.PORT || 3000);
 }
 
 main();
