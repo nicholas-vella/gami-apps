c7fdb97f373ddb63acb57c09d7279003ef006c7d
Sean Mangio <smangio4@gmail.com>
2019-10-04 16:08:20 +1000

    feat: handle case where python app dir does not exist

--- src/utils/load-python-apps/index.ts
+++ src/utils/load-python-apps/index.ts
@@ -1,7 +1,12 @@
+import fs = require('fs-extra');
+
 import runPythonApp from '../run-python-app';
 
 const loadPythonApps = apps => {
-  apps.map(app => runPythonApp(`./src/apps/${app}/index.py`));
+  apps.map(app => {
+    const path = `./src/apps/${app}/index.py`;
+    fs.pathExists(path, (err, exists) => (exists ? runPythonApp(path) : console.log(`Could not find app ${app}`)));
+  });
 };
 
 export default loadPythonApps;
